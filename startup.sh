bf1c0b78955ec0f0fcd82e9c25430e02200a190c
showtime <showtime@ecp-portal-audio.localdomain>
2018-08-30 11:43:44 -0700

    updates to startup script and bitwig project

--- startup.sh
+++ startup.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-modprobe snd-virmidi snd_index=1
+# create virtual midi ports
+# modprobe snd-virmidi snd_index=1
+
+# moved permanently to startup modules with:
+# echo "snd-virmidi snd_index=1" |sudo tee -a /etc/modules
+
+#start up bitwig portal
+
+nohup bitwig-studio /home/showtime/PortalJS/PortalBW/Portal.bwproject &>/dev/null &
