afe71fa11cb3dd0a3790cb33254eb7d712e0ee70
showtime <showtime@ecp-portal-audio.localdomain>
2018-08-31 11:40:22 -0700

    Added startup script to boot cleanly into Bitwig Studio

--- startup.sh
+++ startup.sh
@@ -1,11 +1,33 @@
 #!/usr/bin/env bash
 
 # create virtual midi ports
-# modprobe snd-virmidi snd_index=1
-
-# moved permanently to startup modules with:
-# echo "snd-virmidi snd_index=1" |sudo tee -a /etc/modules
+sudo modprobe snd-virmidi snd_index=1
 
 #start up bitwig portal
 
-nohup bitwig-studio /home/showtime/PortalJS/PortalBW/Portal.bwproject &>/dev/null &
+USAGE="Run with 'clean' argument to erase previous Bitwig session info."
+DIRECTORY="/home/showtime/.BitwigStudio/undo-history/"
+
+load_bitwig () {
+  nohup bitwig-studio /home/showtime/PortalJS/PortalBW/Portal.bwproject &>/dev/null & 
+}
+
+# start clean
+if [[ "$1" == "clean" ]]
+then
+  echo "starting Bitwig cleanly"
+  if [[ -d $DIRECTORY ]]
+  then 
+    rm -r $DIRECTORY
+  fi
+  load_bitwig  
+elif [[ "$1" == "" ]]
+then
+  echo "starting Bitwig with last file info"
+  load_bitwig
+else
+  echo $USAGE
+  exit 1 
+fi
+
+
